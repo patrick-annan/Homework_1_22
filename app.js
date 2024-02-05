14cee8a2195bc2074bfe121f897f5954bf99bdca
Patrick Annan <you@example.compatrickannan@hotmail.com>
2024-02-05 18:08:38 -0500

    video 7

--- app.js
+++ app.js
@@ -3,28 +3,38 @@ const express = require('express');
 // express app
 const app = express();
 
+// register view engine
+app.set('view engine', 'ejs');
+
+
 // listen for requests
 app.listen(3000);
 
 app.get('/', (req, res) => {
+  const bolgs = [
+    {title: 'yoshi finds eggs', snippet: 'lorem ipsum'},
+    {title: 'Mario fiunds stars', snippet: 'lorem ipsum'},
+    {title: 'How to defeat Bowser', snippet: 'lorem ipsum'},
+  ];
 
-  // res.send('<p>home page</p>');
-  res.sendFile('./views/index.html'.{ root: __dirname })
+  res.render('index', { title: 'Home', blogs: blogs  });
 });
 
 app.get('/about', (req, res) => {
 
   // res.send('<p>about page</p>');
-  res.sendFile('./views/about.html'.{ root: __dirname })
+  res.render('about', { title: 'About' });
 });
 
-// redirects
-app.get('/about-us', (req, res) => {
-  res.redirect('/about');
-});
+
+
+app.get('/blogs/create', (req,res) => {
+  res.render('create', { title: 'Create a new blog' });
+})
 
 
 // 404 page
 app.use((req, res) => {
-  res.sendFile('./views/404.html'.{ root: __dirname }) 
-})
+  res.status(404).render('404', { title: '404' });
+});
+
